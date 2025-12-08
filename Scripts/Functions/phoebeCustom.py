e0ae7f24b2f85fad7936beb388feb8b768cb612d
Noah <noahsims1@gmail.com>
2025-12-08 14:21:06 +0000

    Changed generateLC to explore q (R) & T space + added PHOBEBE flux normalise

--- Scripts/Functions/phoebeCustom.py
+++ Scripts/Functions/phoebeCustom.py
@@ -1,19 +1,22 @@
 from phoebe.parameters import ParameterSet as phb
 from lightkurve import LightCurve as lk
+import numpy as np
+from copy import copy
 
 def saveSimulation(bSystem, filename, filenameFits=None, compact=True):
-    """Searches and downloads the MAST data archive for lightcurves. Will save as a .fits file.
+    """All in one save function for PHOEBE simulations. 
+    Saves both as a .phoebe file (to load into PHOEBE) and .fits files for each model (to load into Lightkurve).
     
     Parameters
     ----------
-    bSystem : Phoebe ParameterSet
+    bSystem : PHOEBE Bundle
         The phoebe binary simulation.
     filename : str
         The relative or full path to the .phoebe file that will be saved.
     filenameFits: str (optional)
         The relative or full path to the .fits file that will be saved.
         If left blank, this will be the same as filename.
-    compact : Bool (optional)
+    compact : Bool (optional, default=True)
         Decides whether to use compact file-formatting for the .phoebe file.
     """
     
@@ -33,3 +36,28 @@ def saveSimulation(bSystem, filename, filenameFits=None, compact=True):
         times = bSystem['model@times'].get_value()
         fluxes = bSystem['model@fluxes'].get_value()
         lk(time=times, flux=fluxes).to_fits(f'{filenameFits}_lc.fits', overwrite=True)
+        
+def normaliseFluxes(bSystem):
+    """
+    Normalises fluxes in PHOEBE simulations for easier comparison to real data.
+    Returns a Bundle instance with normalised fluxes.
+    
+    Parameters
+    ----------
+    bSystem : PHOEBE Bundle
+        The PHOEBE binary simulation
+        
+    Returns
+    -------
+    bSystemNormalize : PHOEBE Bundle
+        The normalised PHOEBE binary simulation
+    """
+    
+    bSystemNormalize = copy(bSystem)
+    
+    for model in bSystem.models:
+        flux = bSystem[f'{model}@fluxes'].get_value()
+        medium_flux = np.nanmedian(flux)
+        bSystemNormalize[f'{model}@fluxes'].set_value(value=flux/medium_flux, ignore_readonly=True)
+        
+    return bSystemNormalize
